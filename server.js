3197600e78a9bf701fb02379228d600396c693ec
l3alisidahmed <sidoabdelali06@gmail.com>
2023-08-27 23:53:06 +0100

    api to do list

---
+++ server.js
@@ -0,0 +1,47 @@
+let tasks = ['hello', 'sido', 'how', 'are', 'you', '?'];
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+    res.send    (tasks);
+});
+
+app.get("/add/:task", (req, res) => {
+    new_task = [tasks.push(req.params.task)];
+    tasks.concat(new_task);
+    res.send    ("task added");
+});
+
+app.get("/delete/:indice", (req, res) => {
+    let i = 0;
+    const new_tasks = [];
+    
+    while (i < tasks.length) {
+        if (i !== parseInt(req.params.indice)) {
+            new_tasks.push(tasks[i]);
+        }
+        i++;
+    }
+    
+    tasks = new_tasks;
+
+    res.send(tasks);
+});
+
+app.get("/update/:indice/:new_task", (req, res) => {
+    let i = 0;
+
+    while (i < tasks.length) {
+        if (i == parseInt(req.params.indice)) {
+            tasks[i] = req.params.new_task;
+        }
+        i++;
+    }
+
+    res.send(tasks);
+});
+
+
+
